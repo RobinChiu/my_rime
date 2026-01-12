74811beebc86e984e17ab78064672e946a5b7063
Robin Chiu <hcchiu63@gmail.com>
2026-01-12 13:32:09 +0800

    Add bomopofo keyboard and rime-liur.

--- src/components/SimpleKeyboard.vue
+++ src/components/SimpleKeyboard.vue
@@ -12,7 +12,7 @@ import "simple-keyboard/build/css/index.css";
 import { useOsTheme } from "naive-ui"
 import { currentTheme } from '../util'
 
-const KeyboardHideOutlinedSvg = `<svg style="width:24px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24"><path d="M20 3H4c-1.1 0-1.99.9-1.99 2L2 15c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 12H4V5h16v10zm-9-9h2v2h-2zm0 3h2v2h-2zM8 6h2v2H8zm0 3h2v2H8zM5 9h2v2H5zm0-3h2v2H5zm3 6h8v2H8zm6-3h2v2h-2zm0-3h2v2h-2zm3 3h2v2h-2zm0-3h2v2h-2zm-5 17l4-4H8z" fill="currentColor"></path></svg>`
+const KeyboardHideOutlinedSvg = '<svg style="width:24px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24"><path d="M20 3H4c-1.1 0-1.99.9-1.99 2L2 15c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 12H4V5h16v10zm-9-9h2v2h-2zm0 3h2v2h-2zM8 6h2v2H8zm0 3h2v2H8zM5 9h2v2H5zm0-3h2v2H5zm3 6h8v2H8zm6-3h2v2h-2zm0-3h2v2h-2zm3 3h2v2h-2zm0-3h2v2h-2zm-5 17l4-4H8z" fill="currentColor"></path></svg>'
 const KeyboardReturnRoundSvg = '<svg style="width:24px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24"><path d="M19 8v3H5.83l2.88-2.88A.996.996 0 1 0 7.3 6.71L2.71 11.3a.996.996 0 0 0 0 1.41L7.3 17.3a.996.996 0 1 0 1.41-1.41L5.83 13H20c.55 0 1-.45 1-1V8c0-.55-.45-1-1-1s-1 .45-1 1z" fill="currentColor"></path></svg>'
 const SettingsRegularSvg = '<svg style="width:20px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24"><path d="M19.43 12.98c.04-.32.07-.64.07-.98c0-.34-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46a.5.5 0 0 0-.61-.22l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65A.488.488 0 0 0 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1a.566.566 0 0 0-.18-.03c-.17 0-.34.09-.43.25l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98c0 .33.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46a.5.5 0 0 0 .61.22l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.06.02.12.03.18.03c.17 0 .34-.09.43-.25l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65zm-1.98-1.71c.04.31.05.52.05.73c0 .21-.02.43-.05.73l-.14 1.13l.89.7l1.08.84l-.7 1.21l-1.27-.51l-1.04-.42l-.9.68c-.43.32-.84.56-1.25.73l-1.06.43l-.16 1.13l-.2 1.35h-1.4l-.19-1.35l-.16-1.13l-1.06-.43c-.43-.18-.83-.41-1.23-.71l-.91-.7l-1.06.43l-1.27.51l-.7-1.21l1.08-.84l.89-.7l-.14-1.13c-.03-.31-.05-.54-.05-.74s.02-.43.05-.73l.14-1.13l-.89-.7l-1.08-.84l.7-1.21l1.27.51l1.04.42l.9-.68c.43-.32.84-.56 1.25-.73l1.06-.43l.16-1.13l.2-1.35h1.39l.19 1.35l.16 1.13l1.06.43c.43.18.83.41 1.23.71l.91.7l1.06-.43l1.27-.51l.7 1.21l-1.07.85l-.89.7l.14 1.13zM12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4s4-1.79 4-4s-1.79-4-4-4zm0 6c-1.1 0-2-.9-2-2s.9-2 2-2s2 .9 2 2s-.9 2-2 2z" fill="currentColor"></path></svg>'
 const BackspaceRegularSvg = '<svg style="width:24px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24"><path d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H7.07L2.4 12l4.66-7H22v14zm-11.59-2L14 13.41L17.59 17L19 15.59L15.41 12L19 8.41L17.59 7L14 10.59L10.41 7L9 8.41L12.59 12L9 15.59z" fill="currentColor"></path></svg>'
@@ -24,26 +24,44 @@ const micButtonTarget = ref<HTMLElement | null>(null)
 
 const osThemeRef = useOsTheme()
 
-const currentLanguage = computed(() => isEnglish.value ? "engish" : "chinese")
+// 添加注音模式的狀態
+const currentMode = ref<'default' | 'bopomofo'>('default')
+const currentLanguage = computed(() => isEnglish.value ? "english" : "chinese")
 
-const langBtnCn = "<span><b>中</b>/<span style='color: lightgray'>英</span></span>"
-const langBtnEn = "<span><span style='color: lightgray'>中</span>/<b>英</b></span>"
+// 定義各種模式的按鈕顯示樣式
+const langBtnCn = "<span><b>中</b>/<span style='color: lightgray'>英</span>/<span style='color: lightgray'>注</span></span>"
+const langBtnEn = "<span><span style='color: lightgray'>中</span>/<b>英</b>/<span style='color: lightgray'>注</span></span>"
+const langBtnBopomofo = "<span><span style='color: lightgray'>中</span>/<span style='color: lightgray'>英</span>/<b>注</b></span>"
 
-function getDisplay() {
-  const prevIsEnglish = isEnglish.value
-  return {
-    // '{enter}': render(h(NIcon, { ':component': KeyboardReturnRound })),
-    // '{enter}': returnIcon,
-    '{enter}': KeyboardReturnRoundSvg, // '<img src="@sicons/fluent/Money16Regular.svg" />',
-    '{settings}': SettingsRegularSvg, // '<span style="width: 20px;" class="material-icons-outlined">settings</span>',
-    '{bksp}': BackspaceRegularSvg, //  '<span class="material-icons-outlined">backspace</span>',
+// 定義注音的顯示對照表
+const bopomofoDisplay = {
+  "1":"ㄅ", "2":"ㄉ", "3":"ˇ", "4":"ˋ", "5":"ㄓ", "6":"ˊ", "7":"˙", "8":"ㄚ", "9":"ㄞ", "0":"ㄢ", "-":"ㄦ",
+  "q":"ㄆ", "w":"ㄊ", "e":"ㄍ", "r":"ㄐ", "t":"ㄔ", "y":"ㄗ", "u":"ㄧ", "i":"ㄛ", "o":"ㄟ", "p":"ㄣ",
+  "a":"ㄇ", "s":"ㄋ", "d":"ㄎ", "f":"ㄑ", "g":"ㄕ", "h":"ㄘ", "j":"ㄨ", "k":"ㄜ", "l":"ㄠ", ";":"ㄤ",
+  "z":"ㄈ", "x":"ㄌ", "c":"ㄏ", "v":"ㄒ", "b":"ㄖ", "n":"ㄙ", "m":"ㄩ", ",":"ㄝ", ".":"ㄡ", "/":"ㄥ"
+};
+
+function getDisplay(layout: string) {
+  const langBtn =
+    layout === 'eng' ? langBtnEn :
+    layout === 'bopomofo' ? langBtnBopomofo :
+    langBtnCn;
+
+  const defaultDisplay = {
+    '{enter}': KeyboardReturnRoundSvg,
+    '{settings}': SettingsRegularSvg,
+    '{bksp}': BackspaceRegularSvg,
     "{lock}": "caps ⇪",
     "{shift}": "⇧",
-    "{lang}": prevIsEnglish ? langBtnCn : langBtnEn,
-    // "{hide}": `<img src="data:image/svg+xml,${encodeURIComponent(KeyboardHideOutlinedSvg)}" />`
+    "{lang}": langBtn,
     "{hide}": KeyboardHideOutlinedSvg,
-    "{mic}": '', // MicNoneRoundSvg,
+    "{mic}": " ",
   };
+
+  if (currentMode.value === 'bopomofo') {
+    return { ...bopomofoDisplay, ...defaultDisplay };
+  }
+  return defaultDisplay;
 }
 
 export default defineComponent({
@@ -83,28 +101,36 @@ export default defineComponent({
           "{tab} q w e r t y u i o p [ ] \\",
           "{lock} a s d f g h j k l ; ' {enter}",
           "{shift} z x c v b n m , . / {shift}",
-          "{mic} ， {space} 。 {settings} {hide}",
+          "{lang} {mic} ， {space} 。 {settings} {hide}",
         ],
         micOn: [
           "{esc} \u0060 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
           "{tab} q w e r t y u i o p [ ] \\",
           "{lock} a s d f g h j k l ; ' {enter}",
           "{shift} z x c v b n m , . / {shift}",
-          "{mic} ， {space} 。 {settings} {hide}",
+          "{lang} {mic} ， {space} 。 {settings} {hide}",
         ],
         eng: [
           "{esc} \u0060 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
           "{tab} q w e r t y u i o p [ ] \\",
           "{lock} a s d f g h j k l ; ' {enter}",
           "{shift} z x c v b n m , . / {shift}",
-          "{mic} .com {space} @ {settings} {hide}",
+          "{lang} {mic} .com {space} @ {settings} {hide}",
         ],
         shift: [
           "{esc} ~ ! @ # $ % ^ & * ( ) _ + {bksp}",
           "{tab} Q W E R T Y U I O P { } |",
           '{lock} A S D F G H J K L : " {enter}',
           "{shift} Z X C V B N M < > ? {shift}",
-          "{mic} , {space} . {settings} {hide}",
+          "{lang} {mic} , {space} . {settings} {hide}",
+        ],
+        // 注音鍵盤布局
+        bopomofo: [
+          "{esc} \u0060 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
+          "{tab} q w e r t y u i o p [ ] \\",
+          "{lock} a s d f g h j k l ; ' {enter}",
+          "{shift} z x c v b n m , . / {shift}",
+          "{lang} {mic} ， {space} 。 {settings} {hide}",
         ],
       },
       mergeDisplay: true,
@@ -117,7 +143,7 @@ export default defineComponent({
         "{shift}": "⇧",
         "{lang}": currentLanguage.value === "chinese" ? langBtnCn : langBtnEn,
         "{esc}": "esc",
-        "{mic}": " ", // MicNoneRoundSvg,
+        "{mic}": " ",
       }
     });
     nextTick(() => {
@@ -141,15 +167,19 @@ export default defineComponent({
       /**
        * If you want to handle the shift and caps lock buttons
        */
-      if (this.shiftPressed && (button !== "{shift}" && button !== "{lock}")) { 
+      if (this.shiftPressed && (button !== "{shift}" && button !== "{lock}")) {
         this.handleShift(button);
         this.shiftPressed = false;
+      }
+      if (button === "{shift}" || button === "{lock}") {
+        this.handleShift(button);
         return;
       }
-      if (button === "{shift}" || button === "{lock}") this.handleShift(button);
 
-      if (button === "{lang}") this.switchLanguage();
-
+      if (button === "{lang}") {
+        this.switchLanguage();
+        return;
+      }
     },
     handleShift(button: string) {
       if (!this.keyboard) return
@@ -165,12 +195,32 @@ export default defineComponent({
     },
     switchLanguage() {
       if (!this.keyboard) return
+      
+      // 獲取當前布局
       const currentLayout = this.keyboard.options.layoutName;
-      const langToggle = currentLayout === "default" ? "eng" : "default";
+      
+      // 實現三種模式的循環切換：中文 -> 英文 -> 注音 -> 中文
+      let nextLayout;
+      if (currentLayout === "default") {
+        // 中文 -> 英文
+        nextLayout = "eng";
+        isEnglish.value = true;
+        currentMode.value = 'default';
+      } else if (currentLayout === "eng") {
+        // 英文 -> 注音
+        nextLayout = "bopomofo";
+        isEnglish.value = false;
+        currentMode.value = 'bopomofo';
+      } else {
+        // 注音 -> 中文
+        nextLayout = "default";
+        isEnglish.value = false;
+        currentMode.value = 'default';
+      }
 
       this.keyboard.setOptions({
-        layoutName: langToggle,
-        display: getDisplay()
+        layoutName: nextLayout,
+        display: getDisplay(nextLayout)
       });
       this.remountRecorderButton()
     }
@@ -353,6 +403,18 @@ export default defineComponent({
 
 .hg-button.hg-button-space {
   flex: 4;
+}
+
+/* 注音鍵盤空格鍵樣式 - 作為第一聲使用 */
+.simple-keyboard .hg-layout-bopomofo .hg-button-space {
+  background-color: rgba(240, 240, 240, 95%);
+  border: 1px solid rgba(200, 200, 200, 50%);
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
+}
+
+.simple-keyboard.darkTheme .hg-layout-bopomofo .hg-button-space {
+  background-color: rgba(255, 255, 255, 0.15);
+  border: 1px solid rgba(255, 255, 255, 0.1);
 }
 
 .hg-button.hg-standardBtn[data-skbtn="，"] {
